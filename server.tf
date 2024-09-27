bc6c1a2267b1c736f851022e43893b6384372f8d
Raman Khanna <89792896+ramannkhanna2@users.noreply.github.com>
2024-09-27 14:32:18 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -12,6 +12,7 @@ region = "us-east-1"
 }
 resource "aws_instance" "myawsserver" {
   ami = "ami-0e54eba7c51c234f6"
+  vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
   instance_type = "t2.micro"
   key_name = "raman-import"
 
